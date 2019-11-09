8bfede8e11a73e350f14d0ef8f069d985e31e320
mini0n <mini0n.000@gmail.com>
2019-11-09 13:16:44 -0600

    Rpec and login tests

--- Gemfile
+++ Gemfile
@@ -37,10 +37,18 @@ gem 'jwt'
 # Use Active Model Serializers
 gem 'active_model_serializers', '~> 0.10.0'
 
+# Awesome print
+gem 'awesome_print'
+
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  # Rspec
+  gem 'rspec-rails'
+  gem 'database_cleaner'
 end
 
 group :development do
---
+++ spec/controllers/authentication_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticationController, type: :controller do
+
+  describe '#login' do
+    context 'valid login' do
+      let(:valid_login) { { user: { username: 'admin', password: 'admin' } } }
+      let(:invalid_login) { { user: { username: 'bad', password: 'login' } } }
+
+      it 'logins' do
+        post 'login', params: valid_login
+        result = JSON.parse(response.body)
+
+        expect(response.status).to eq 200
+        expect(result['username']).to eq valid_login[:user][:username]
+        expect(result['token'].present?).to be true
+      end
+
+      it 'fails' do
+        post 'login', params: invalid_login
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq 'null'
+      end
+    end
+
+
+
+
+  end
+end
